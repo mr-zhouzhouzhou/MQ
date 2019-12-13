2a421bc4704cb32053622af4ef42cf0f975f3989
zhouwang_i <zhouwang_i&didiglobal.com>
2019-12-13 15:35:13 +0800

    添加constant

---
+++ constant/dltag.go
@@ -0,0 +1,11 @@
+package constant
+
+const (
+
+	//上报至metrics的统计曲线名称
+	METRICS_NAME_BOUNTY = "strategy_bounty_rt"
+
+	//记录public日志，使用的常量
+	PUBLIC_LOG_KEY = "g_strategy_bounty"
+	PUBLIC_LOG_TAG = "BOUNTY"
+)
